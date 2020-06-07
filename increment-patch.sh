ddf9c3cb07313832ea6431e9d874d612df04700f
Ginjiro Ueta <g.ueta@mmj.co.jp>
2020-06-07 10:39:49 +0900

    develop tagのpushを実装

--- increment-patch.sh
+++ increment-patch.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 set -xe
 
-latest=$(git tag -l --sort=v:refname | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+" | sort -r | head -n 1)
+latest=$(git tag -l --sort=v:refname | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+" | sort -r -V | head -n 1)
 current_patch=$(echo $latest | grep -o "[0-9]\+$")
 patch=$(($current_patch + 1))
 major_minor=$(echo $latest | grep -o "^v[0-9]\+\.[0-9]\+\.")
 new=$major_minor$patch
 
 git tag $new
-git push --tags
+curl -v -d @- --request POST \
+        --url https://api.github.com/repos/${GITHUB_REPOSITORY}/git/tags \
+        --header "Authorization: token $GITHUB_TOKEN" \
+        --header 'Content-Type: application/json' \
+        --header "Accept: application/vnd.github.v3+json" <<EOF
+{
+  "tag": "${new}",
+  "message": "This tag created by Github Actions",
+  "object": "${GITHUB_SHA}",
+  "type": "commit"
+}
+EOF
+
